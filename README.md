2b4b2b9b5b9396c8d0ea06fff26108ecffeaf427
James L <somesocks@gmail.com>
2018-08-21 20:31:31 +0200

    changing spec, using internal cache first

--- README.md
+++ README.md
@@ -2,7 +2,7 @@
 
 libstub is a tiny UMD module that uses the global namespace to dynamically link dependencies between JS bundles.
 
-Why? Mostly to help link together JS, independent of how they were built.
+Why? To help link together JS lebs, independent of how they were built.
 
 # Usage
 
@@ -34,35 +34,3 @@ Why? Mostly to help link together JS, independent of how they were built.
 
 # API
 
-<a name="libstub"></a>
-
-## libstub : <code>object</code>
-**Kind**: global namespace  
-
-* [libstub](#libstub) : <code>object</code>
-    * [.export](#libstub.export)
-    * [.import](#libstub.import)
-
-<a name="libstub.export"></a>
-
-### libstub.export
-export a module to use
-
-**Kind**: static property of [<code>libstub</code>](#libstub)  
-
-| Param | Description |
-| --- | --- |
-| name | the name of the module |
-| module | the module |
-
-<a name="libstub.import"></a>
-
-### libstub.import
-import a module already exported
-
-**Kind**: static property of [<code>libstub</code>](#libstub)  
-
-| Param | Description |
-| --- | --- |
-| name | the name of the module |
-| ignoreMissing | if true, do not throw an error when the module is not found |
--- dist/libstub.js
+++ dist/libstub.js
@@ -1,7 +1,3 @@
-/* globals define self */
-/** @namespace libstub */
-
-
 (function (_r, _f) {
 	if (typeof define === 'function' && define.amd) {
 		define(function () { return (_r.libstub = _f(_r)); });
@@ -11,28 +7,26 @@
 		_r.libstub = _f(_r);
 	}
 }(typeof self !== 'undefined' ? self : this, function (_r) {
-	return {
-		/**
-		* Export a module for use
-		* @name export
-		* @param name - the name of the module
-		* @param module - the module
-		* @memberof libstub
-		*/
-		export: function (_n, _m) {
-			if (!_r[_n]) { _r[_n] = _m; }
-		},
 
-		/**
-		* Import a module already exported
-		* @name import
-		* @param name - the name of the module
-		* @param ignoreMissing - if true, do not throw an error when the module is not found
-		* @memberof import
-		*/
-		import: function (_n, _i) {
-			if (!_r[_n] && !_i) { throw new Error('libstub: no module ' + _n); }
-			return _r[_n];
-		},
-	};
+	function _ls() {
+		this._cache = {};
+
+		this.import = function (_n) {
+			var _l = this._cache[_n] || _r[_n];
+			if (!_l) { throw new Error('libstub: no module ' + _n); }
+			this._cache[_n] = _l;
+			return _l;
+		};
+
+		this.export = function (_n, _l) {
+			_l = this._cache[_n] || _r[_n] || _l;
+			this._cache[_n] = _l;
+			return _l;
+		};
+
+		return this;
+	}
+
+	_r.libstub = _r.libstub || new _ls();
+	return _r.libstub;
 }));
