72a1376a617155b880b4cfdb22f5f4f5266e9a98
James L <somesocks@gmail.com>
2024-08-11 15:15:05 +0200

    - adding globalThis check

--- src/libstub.js
+++ src/libstub.js
@@ -6,58 +6,61 @@
 	} else {
 		_r.libstub = _f(_r);
 	}
-}(typeof self !== 'undefined' ? self : this, function (_r) {
+}(
+	typeof globalThis !== 'undefined' ? globalThis : (typeof self !== 'undefined' ? self : this),
+	function (_r) {
 
-	function _ls() {
-		var self = this;
-		self._cache = {};
+		function _ls() {
+			var self = this;
+			self._cache = {};
 
-		self.import = function (n, l) {
-			if (n && typeof n === 'object') {
-				if (Object.prototype.toString.call(n) === '[object Array]') {
-					for (var i = 0; i < n.length; i++) {
-						n[i] = self.import(n[i]);
-					}
-				} else {
-					for (var k in n) {
-						if (n.hasOwnProperty(k)) {
-							n[k] = self.import(k, n[k]);
+			self.import = function (n, l) {
+				if (n && typeof n === 'object') {
+					if (Object.prototype.toString.call(n) === '[object Array]') {
+						for (var i = 0; i < n.length; i++) {
+							n[i] = self.import(n[i]);
+						}
+					} else {
+						for (var k in n) {
+							if (n.hasOwnProperty(k)) {
+								n[k] = self.import(k, n[k]);
+							}
 						}
 					}
+					return n;
 				}
-				return n;
-			}
 
-			l = self._cache[n] || _r[n] || l;
-			if (!l) { throw new Error('libstub: no module ' + n); }
-			if (l.__libstub_lazy) { l = l(self); }
-			self._cache[n] = l;
-			return l;
-		};
+				l = self._cache[n] || _r[n] || l;
+				if (!l) { throw new Error('libstub: no module ' + n); }
+				if (l.__libstub_lazy) { l = l(self); }
+				self._cache[n] = l;
+				return l;
+			};
 
-		self.export = function (n, l) {
-			if (n && typeof n === 'object') {
-				for (let k in n) {
-					if (n.hasOwnProperty(k)) {
-						n[k] = self.export(k, n[k]);
+			self.export = function (n, l) {
+				if (n && typeof n === 'object') {
+					for (let k in n) {
+						if (n.hasOwnProperty(k)) {
+							n[k] = self.export(k, n[k]);
+						}
 					}
+					return n;
 				}
-				return n;
-			}
 
-			l = self._cache[n] || _r[n] || l;
-			self._cache[n] = l;
-			return l;
-		};
+				l = self._cache[n] || _r[n] || l;
+				self._cache[n] = l;
+				return l;
+			};
 
-		self.lazy = function (l) {
-			l.__libstub_lazy = true;
-			return l;
-		};
+			self.lazy = function (l) {
+				l.__libstub_lazy = true;
+				return l;
+			};
 
-		return self;
+			return self;
+		}
+
+		_r.libstub = _r.libstub || new _ls();
+		return _r.libstub;
 	}
-
-	_r.libstub = _r.libstub || new _ls();
-	return _r.libstub;
-}));
+));
